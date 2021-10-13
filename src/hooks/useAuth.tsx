30ec55a38eee408c18f1a12c6a1f9d083fb4ae4f
onur <onurpolatplt@gmail.com>
2021-10-13 18:02:10 +0200

    Fix casing discrepancies.

--- src/hooks/useAuth.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { AuthContext } from "../assets/useContext/AuthProvider";
-
-const useAuth = () => {
-  const context = useContext(AuthContext);
-  return context;
-};
-
-export default useAuth;
